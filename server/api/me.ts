6da94821247c8c3d86f19800d808502253cb5687
MulkiHafizh <ukiyo142@gmail.com>
2023-07-06 17:16:41 +0700

    profile image issue

--- server/api/me.ts
+++ server/api/me.ts
@@ -4,7 +4,7 @@ export default eventHandler(async () => {
   const response = await getMe();
   const me = await response.json();
   const name = me.display_name;
-  const image = me.images[0].url;
+  const image = me.images[1].url;
   const url = me.external_urls.spotify;
 
   return {
