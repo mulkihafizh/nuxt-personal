4b7cacd5c7a39dafc319f06e00bd428628dff187
MulkiHafizh <ukiyo142@gmail.com>
2023-08-07 13:59:21 +0700

    testing

--- app.vue
+++ app.vue
@@ -1,3 +1,21 @@
+<script setup>
+useHead({
+  script: [
+    {
+      src: "https://www.googletagmanager.com/gtag/js?id=G-1V9QGGNH8E",
+      async: true,
+      children: `        
+      window.dataLayer = window.dataLayer || [];
+        function gtag() {
+          dataLayer.push(arguments);
+        }
+        gtag("js", new Date());
+
+        gtag("config", "G-1V9QGGNH8E");`,
+    },
+  ],
+});
+</script>
 <template>
   <div
     class="font-main duration-300 dark:bg-zinc-950 min-h-screen dark:text-white bg-gray-100"
